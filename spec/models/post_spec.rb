ee495b4dd2d427a2fa48ad636e037f53d6118fc2
Lucile Jones <jones.lucile@gmail.com>
2024-02-07 13:20:33 -0700

    followed notes for first test

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context 'with valid attributes' do
+    it 'is valid' do
+      post = Post.new(title: 'My First Post', content: 'Hello, world!')
+      expect(post).to be_valid
+    end
+  end
+
+  context 'with invalid attributes' do
+    it 'is invalid without a title' do
+      post = Post.new(content: 'Hello, world!')
+      expect(post).to be_invalid
+    end
+  end
+end
